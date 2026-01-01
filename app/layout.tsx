7212e0033d45461bd71ca26f4323ad7c9d295bab
subhanigc <masoodsubahani01@gmail.com>
2026-01-01 23:56:54 +0500

    Fix Tailwind PostCSS error by downgrading to v3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Inter } from "next/font/google"
+
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
 
-const _geist = Geist({ subsets: ["latin"] })
-const _geistMono = Geist_Mono({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] })
+
 
 export const metadata: Metadata = {
   title: "v0 App",
